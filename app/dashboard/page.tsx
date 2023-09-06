b7acb28742b0bf7f8a7743d2c18b2ad364ca64c0
frewcm <frewbefekadu32@gmail.com>
2023-09-06 07:45:37 +0300

    initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Button from "@/components/Button";
+import { FC } from "react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return (
+    <div>
+      <Button>hello</Button>
+    </div>
+  );
+};
+
+export default page;
